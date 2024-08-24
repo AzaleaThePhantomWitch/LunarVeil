7b9b3c290eeb649fe1b38be42e6862260ea736e9
Scolon <49799704+sscolon@users.noreply.github.com>
2024-08-24 16:19:05 -0400

    localization

--- LunarVeilConfig.cs
+++ LunarVeilConfig.cs
@@ -12,11 +12,10 @@ namespace LunarVeil
     {
         public override ConfigScope Mode => ConfigScope.ClientSide;
 
-        [Header("SkiesToggle")]
+        [Header("Visuals")]
         [DefaultValue(true)] // This sets the configs default value. // Marking it with [ReloadRequired] makes tModLoader force a mod reload if the option is changed. It should be used for things like item toggles, which only take effect during mod loading
         public bool SkiesToggle;
 
-        [Header("WaterToggle")]
         [DefaultValue(true)] // This sets the configs default value.// Marking it with [ReloadRequired] makes tModLoader force a mod reload if the option is changed. It should be used for things like item toggles, which only take effect during mod loading
         public bool WatersToggle;
     }
