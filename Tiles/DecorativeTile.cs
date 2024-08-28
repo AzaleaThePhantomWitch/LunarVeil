c1471f5395ab70a882673f00f45d5352c714606d
Scolon <49799704+sscolon@users.noreply.github.com>
2024-08-27 23:40:52 -0400

    wall  stuff

---
+++ Tiles/DecorativeTile.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace LunarVeil.Tiles
+{
+
+    internal abstract class DecorativeTile : ModTile
+    {
+        public Color StructureColor { get; set; }
+        public override void SetStaticDefaults()
+        {
+            StructureColor = Color.White;
+            Main.tileSolid[Type] = true;
+            Main.tileMerge[Type][Type] = true;
+            Main.tileBlockLight[Type] = true;
+            Main.tileBlendAll[Type] = true;
+            Main.tileBlockLight[Type] = true;
+            AddMapEntry(Color.White);
+        }
+
+        public override bool PreDraw(int i, int j, SpriteBatch spriteBatch)
+        {
+            string texturePath = Texture + "_S";
+            Texture2D texture = ModContent.Request<Texture2D>(texturePath).Value;
+            int textureWidth = texture.Width;
+            int textureHeight = texture.Height;
+
+            Vector2 drawPos = (new Vector2(i, j) + Systems.Tiling.MultitileHelper.TileAdj) * 16;
+            Vector2 drawOrigin = new Vector2(textureWidth / 2, textureHeight);
+            spriteBatch.Draw(texture, drawPos - Main.screenPosition, null, StructureColor, 0, drawOrigin, 1, SpriteEffects.None, 0);
+            return base.PreDraw(i, j, spriteBatch);
+        }
+    }
+
+    public abstract class DecorativeWallItem : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.width = 12;
+            Item.height = 12;
+            Item.maxStack = Item.CommonMaxStack;
+            Item.useTurn = true;
+            Item.autoReuse = true;
+            Item.useAnimation = 10;
+            Item.useTime = 10;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.consumable = true;
+        }
+
+        public override bool PreDrawInInventory(SpriteBatch spriteBatch, Vector2 position, Rectangle frame, Color drawColor, Color itemColor, Vector2 origin, float scale)
+        {
+            DecorativeWall decorativeWall = ModContent.GetModWall(Item.createWall) as DecorativeWall;
+            if (decorativeWall == null)
+                return false;
+            decorativeWall.DrawItem(spriteBatch, position, frame, drawColor, itemColor, origin, scale);
+            return false;
+        }
+    }
+
+    internal abstract class DecorativeWall : ModWall
+    {
+        public Color StructureColor { get; set; }
+        public override string Texture => "LunarVeil/Tiles/InvisibleWall";
+        public string StructureTexture { get; set; }
+        public override void SetStaticDefaults()
+        {
+            StructureColor = Color.White;
+            StructureTexture = this.GetType().FullName + "_S";
+            StructureTexture = StructureTexture.Replace(".", "/");
+            Main.wallHouse[Type] = false;
+            AddMapEntry(new Color(200, 200, 200));
+        }
+
+        public void DrawItem(SpriteBatch spriteBatch, Vector2 position, Rectangle frame, Color drawColor, Color itemColor, Vector2 origin, float scale)
+        {
+            Texture2D texture = ModContent.Request<Texture2D>(StructureTexture).Value;
+            int textureWidth = texture.Width;
+            int textureHeight = texture.Height;
+            Vector2 drawOrigin = new Vector2(textureWidth / 2, textureHeight / 2);
+            spriteBatch.Draw(texture, position, null, drawColor, 0, drawOrigin, scale * 0.5f, SpriteEffects.None, 0);
+        }
+
+        public override void PostDraw(int i, int j, SpriteBatch spriteBatch)
+        {
+            base.PostDraw(i, j, spriteBatch);
+            Texture2D texture = ModContent.Request<Texture2D>(StructureTexture).Value;
+            int textureWidth = texture.Width;
+            int textureHeight = texture.Height;
+
+            Vector2 drawPos = (new Vector2(i, j) + Systems.Tiling.MultitileHelper.TileAdj) * 16;
+            Vector2 drawOrigin = new Vector2(textureWidth / 2, textureHeight);
+            spriteBatch.Draw(texture, drawPos - Main.screenPosition, null, StructureColor, 0, drawOrigin, 1, SpriteEffects.None, 0);
+        }
+    }
+}
