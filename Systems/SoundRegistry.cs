78b9295d2748f78fcaa4143e0ad737af497215c5
AzaleaThePhantomWitch <atych44@gmail.com>
2024-09-01 14:05:03 -0500

    grergdrgerer

--- Systems/SoundRegistry.cs
+++ Systems/SoundRegistry.cs
@@ -1,4 +1,11 @@
-﻿using Terraria.Audio;
+﻿using Terraria;
+using Terraria.Audio;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria.GameContent.ItemDropRules;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ModLoader.Utilities;
 
 namespace LunarVeil.Systems
 {
@@ -8,5 +15,8 @@ namespace LunarVeil.Systems
 
         //Example how to add
         public static SoundStyle StormDragonLightningRain => new SoundStyle($"{RootAssetPath}StormDragon_LightingZap");
+        public static SoundStyle BowCharge => new SoundStyle($"{RootAssetPath}BowCharge1");
+        public static SoundStyle BowShoot => new SoundStyle($"{RootAssetPath}BowShot1");
+
     }
 }
