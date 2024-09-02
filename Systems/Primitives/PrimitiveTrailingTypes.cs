8a03cc3c44bce68d3cc87e9685b38586d75ee503
Zenovia <calamityjewelstudios@gmail.com>
2024-09-02 00:14:42 -0400

    gren

--- Systems/Primitives/PrimitiveTrailingTypes.cs
+++ Systems/Primitives/PrimitiveTrailingTypes.cs
@@ -975,4 +975,40 @@ namespace LunarVeil.Systems.Primitives
             managed.RemoveAll(n => n.IsDisposed);
         }
     }
-}
+
+ 
+        public class PrimitiveDrawing : HookGroup
+        {
+            // Should not interfere with anything.
+            public override void Load()
+            {
+                if (Main.dedServ)
+                    return;
+
+                On_Main.DrawDust += DrawPrimitives;
+            }
+
+            private void DrawPrimitives(On_Main.orig_DrawDust orig, Main self)
+            {
+                orig(self);
+
+                if (Main.gameMenu)
+                    return;
+
+                Main.graphics.GraphicsDevice.BlendState = BlendState.AlphaBlend;
+
+                for (int k = 0; k < Main.maxProjectiles; k++) // Projectiles.
+                {
+                    if (Main.projectile[k].active && Main.projectile[k].ModProjectile is IDrawPrimitive)
+                        (Main.projectile[k].ModProjectile as IDrawPrimitive).DrawPrimitives();
+                }
+
+                for (int k = 0; k < Main.maxNPCs; k++) // NPCs.
+                {
+                    if (Main.npc[k].active && Main.npc[k].ModNPC is IDrawPrimitive)
+                        (Main.npc[k].ModNPC as IDrawPrimitive).DrawPrimitives();
+                }
+            }
+        }
+    }
+
